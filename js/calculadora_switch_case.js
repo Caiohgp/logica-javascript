f653bb54d7b85e99ff109be657ce8cdf2c27e212
Caiohgp <caio.102008@gmail.com>
2021-09-10 22:08:20 -0300

    Mudança aleatória

--- js/calculadora_switch_case.js
+++ js/calculadora_switch_case.js
@@ -1,6 +1,6 @@
 function calcular2(){
     var valor1, valor2, operacao, resultado
-    alert("Olá, esse é a minha calculadora :D")
+    alert("Calculadora dahora")
     alert("Vou te pedir 2 números, depois você vai me dizer a operação que deseja realizar")
     valor1 = prompt("Vamos lá, me diga o primeiro número")
     valor2 = prompt("Agora me diga o segundo número")
